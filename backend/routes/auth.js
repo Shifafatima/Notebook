24031efe85874662524d11fe9f12aeb4f3aa8282
Shifafatima <shifafatima586@gmail.com>
2024-07-08 10:54:47 +0500

    Password hashing using salt done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,25 +2,45 @@ const express=require('express');
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const router=express.Router()
+const bcrypt= require('bcrypt')
 
 
-router.post('/',[body('name','Enter a valid Name').isLength({min:3}),
+
+router.post('/createUser',[body('name','Enter a valid Name').isLength({min:3}),
     body('email','Enter a valid email').isEmail(),
     body('password','Password must be atleast 3 characters').isLength({min:3})
 ],
-(req,res)=>{
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({erros:errors.array()});
+async (req, res) => {
+    const errors = validationResult(req);
+    // If there are errors, return bad request and the errors
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name:req.body.name,
-        password:req.body.password,
-        email:req.body.email
-    }).then(user=>res.json(user))
-    .catch(error=>{console.log(error)
-        res.json({err:"Please Enter a unique value for Email",message:error.message})
-    });
-})
 
-module.exports=router
+    try {
+      // Check whether a user with the same email already exists
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "Sorry, a user with this email already exists" });
+      }
+      //Use bcrypt.js for password hashing
+      const salt= await bcrypt.genSalt(10);
+      const secPass= await bcrypt.hash(req.body.password,salt)
+
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email
+      });
+
+      // Respond with the created user
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
